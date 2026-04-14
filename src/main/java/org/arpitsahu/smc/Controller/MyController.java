b82ff7285cc167d17940d203f4d671738bbaf710
Arpit Sahu <arpitsahu2203@gmail.com>
2026-04-14 06:54:47 +0530

    Understanding page layout

--- src/main/java/org/arpitsahu/smc/Controller/MyController.java
+++ src/main/java/org/arpitsahu/smc/Controller/MyController.java
@@ -1,4 +1,18 @@
 package org.arpitsahu.smc.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//will return a html page upon calls
+@RequestMapping("/SMC")
 public class MyController {
+
+    @GetMapping("/home")
+    public String Home(Model mod){
+        mod.addAttribute("name","Arpit Sahu");
+        mod.addAttribute("work","Java Developer");
+        return "home";
+    }
 }
--- src/test/java/org/arpitsahu/smc/SmcApplicationTests.java
+++ src/test/java/org/arpitsahu/smc/SmcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SmcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
